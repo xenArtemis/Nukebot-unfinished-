5d1556de0ef17aec03f70d6321027b5d210695b7
xenArtemis <funniest656@gmail.com>
2024-06-23 10:31:11 +0300

    Revamp soon

--- code.py
+++ code.py
@@ -1,14 +1,1 @@
-# funny bit, code is very messy
-import random
-import aiohttp
-
-names = [
-    "Artemis"
-    
-]
-import discord
-token = "bitchass token"
-from discord.ext import commands
-message = "everyone" 
-
-artemis = commands.Bot(command_prefix='.'
+# revamping this, didnt like it
