9ede8da7913b604bbaed2d9f9e077c1431f67fcf
xenArtemis <funniest656@gmail.com>
2024-06-23 09:36:30 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
+# funny bit, code is very messy
+import random
+import aiohttp
 
+names = [
+    "Artemis"
+    
+]
+import discord
+token = "bitchass token"
+from discord.ext import commands
+message = "everyone" 
