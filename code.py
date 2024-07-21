ac7192a2f3cc076c453a157a9841106cf1d67950
xenArtemis <funniest656@gmail.com>
2024-07-21 23:28:30 +0300

    updated code.py
    added basics

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
-# revamping this, didnt like it
+import random
+import aiohttp
+from discord.ext import commands
+import asyncio
+
+names = [
+    "raped by artemis",
+    "buttfucked",
+    "kys faggot",
+    "bumraped by artemis",
+    "heil artemis",
+    "oh emm gee",
+    "pee ohh ess"
+]
+
+token = "your retarded token"
+
+phobia = commands.Bot(command_prefix="+", intents=discord.Intents.all())
+
